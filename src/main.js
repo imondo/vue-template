4efe964e21940ce326f3c0b89756e0572e4ed20a
Mondo <568197478@qq.com>
2019-06-03 17:28:59 +0800

    element插件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import '@/components/ElementUI';
 import router from './router';
 import store from './store';
+
+import './plugins/element';
 import './permission';
 
 import './styles/index.scss';
