1d38fce117eaa04e61201731292fbc10dafa8b15
Mondo <568197478@qq.com>
2020-03-17 09:48:15 +0800

    refactor: 配置优化

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@ const local = require('./../build/setting');
 
 const host = local();
 
-const port = 8080;
 /**
  * 项目启动配置
  * publicPath 项目根路径
@@ -10,24 +9,29 @@ const port = 8080;
  * port 本机端口
  * proxy webpack代理
  */
-const proxyTarget = 'https://test.com';
-
-module.exports = {
+const config = {
   publicPath: '/vue-template/',
   host: host || 'http://localhost',
-  port,
+  prot: 8080,
+  proxyTarget: 'https://test.com'
+}
+
+module.exports = {
+  publicPath: config.publicPath,
+  host: config.host,
+  port: config.prot,
   proxy: {
     '/api/mock': {
-      target: `http://${host}:${port}`,
+      target: `http://${config.host}:${config.prot}`,
       changeOrigin: true,
       pathRewrite: {'^/api': ''}
     },
     '/test/api/': {
-      target: proxyTarget,
+      target: config.proxyTarget,
       changeOrigin: true
     },
     [process.env.VUE_APP_BASE_API]: {
-      target: `http://localhost:${port}${publicPath}`,
+      target: `http://localhost:${config.port}${config.publicPath}`,
       changeOrigin: true,
       pathRewrite: {
         ['^' + process.env.VUE_APP_BASE_API]: ''
