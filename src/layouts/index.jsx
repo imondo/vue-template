20cf3c32851392514ddd64463810116d8c715db0
wuyouwen <568197478@qq.com>
2021-08-20 15:38:56 +0800

    style: 收缩修改

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -4,6 +4,7 @@ import Header from './Header/Header.jsx';
 import Siderbar from './Sidebar/Sidebar';
 import './index.less';
 import { useMenu } from '@/hooks/useMenu';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   setup() {
@@ -15,11 +16,13 @@ export default defineComponent({
       )
     };
 
+    const { getters } = useStore();
+
     const { menu } = useMenu();
     return () => (
       <div class="app-box">
         <Siderbar menu={menu} />
-        <section class="app-main">
+        <section class={`app-main ${getters.collapsed ? 'app-main-hide' : ''}`}>
           <Header />
           <main class="app-container">
             <RouterView v-slots={slots}></RouterView>
